048dab61bcd97bc0fb94e4fc6f6db63a77dbeb5c
zhongren <zhrren@qq.com>
2023-12-23 10:09:41 +0800

    update

--- lib/config/MsgHandler.dart
+++ lib/config/MsgHandler.dart
@@ -1,5 +1,6 @@
 import "package:app/base/kernel/Logger.dart";
 import "package:app/base/store/AuthStore.dart";
+import "package:app/base/store/IdentityStore.dart";
 import "package:app/uikit/clip/ClipMsgHandler.dart";
 import "package:dartx/dartx.dart";
 import "package:flutter/material.dart";
@@ -8,8 +9,9 @@ import "package:injectable/injectable.dart";
 @Injectable()
 class MsgHandler extends ClipMsgHandler {
   AuthStore authStore;
+  IdentityStore identityStore;
 
-  MsgHandler(this.authStore);
+  MsgHandler(this.authStore, this.identityStore);
 
   @override
   void handle(BuildContext context, Map<String, dynamic> msg, void Function(Map<String, dynamic> payload) reply) {
@@ -17,8 +19,10 @@ class MsgHandler extends ClipMsgHandler {
     var payload = msg.getOrElse("payload", () => {}) as Map<String, dynamic>;
     switch (name) {
       case "setIdentity":
+        var id = payload.getOrElse("id", () => "") as String;
         var token = payload.getOrElse("token", () => "") as String;
         authStore.login(token);
+        identityStore.save(id: id);
 
         logger.i("setIdentity, token: $token");
         reply({});
